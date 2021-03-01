9ea9afe59d6d2b3ced8ea2460e4fd19dd7226f7f
Peter Heilbo Ratgen <peter@pratgen.dk>
2021-03-01 08:32:16 +0100

    improvements

--- labs/week8/Homework/search.py
+++ labs/week8/Homework/search.py
@@ -55,7 +55,7 @@ def EXPAND(node):
 Insert node in to the queue (fringe).
 '''
 def INSERT(node, queue):
-   queue.insert(0, node)
+   queue.append(node)
    return queue
     
 
@@ -65,7 +65,7 @@ def INSERT(node, queue):
 '''
 def INSERT_ALL(list, queue):
     for item in list:
-        queue.insert(0, item)
+        queue.append(item)
     return queue
 
 
@@ -79,65 +79,79 @@ def REMOVE_FIRST(queue):
 Successor function, mapping the nodes to its successors
 '''
 def successor_fn(state):  # Lookup list of successor states
-    print("state input " + str(state))
-    children = STATE_SPACE[state]  # successor_fn( 'C' ) returns ['F', 'G']
-    print("initial children" + str(children))
+    print("enter state " + str(state))
+    children = STATE_SPACE[state][:]  # successor_fn( 'C' ) returns ['F', 'G']
 
-    illegal_states = [state] + [INITIAL_STATE]
+    illegal_states = [state]# remove the state in question and the initial state
     for remove_state in illegal_states:
         if remove_state in children[:]: 
-            print('remove state' + str(remove_state))
             children.remove(remove_state)
 
-    for STATE in children[:]: 
-        for i in range(1,4):
-            if state[i] == 'E' and STATE[i] != state[i]: #wolf, goat and cabbage should not travel back
-                print("they should not travelback\n removing state: " + str(STATE) + "\nfrom " + str(children))
-                children.remove(STATE)
-                break
-            if (STATE[1] == STATE[2] and STATE[0] != STATE[1]) or (STATE[2] == STATE[3] and STATE[0] != STATE[2]):
-                print("they cannot be on same " + str(STATE))
-                children.remove(STATE)
-                break
+    problem_constraints(children)
 
+    check_amount_of_travelers(state, children)
 
-        if not in_list(children, STATE): continue
+    check_farmer_place(state, children)
 
-        if STATE.count('E') - state.count('E') > 2 : #no more than two subjects can travel at once
-            print("no more than two subjects can travel\n removing state: " + str(STATE) + "\nfrom " + str(children))
-            children.remove(STATE)
+    print("Return children")
+    print(str(children))
 
-        if not in_list(children, STATE): continue
-
-        '''
-        print("w state count" + str(STATE.count('W') - state.count('W')))
-        if STATE.count('W') - state.count('W') not in [-2, -1, 1, 2]:
-            children.remove(STATE)
-        '''
-
-        if not in_list(children, STATE): continue
-
-        if state[0] == 'E':
-            for i in range(1,4):
-                if state[i] != STATE[i]:
-                    print("farmer needs to be on west side to travel\n removing state: " + str(STATE) + "\nfrom " + str(children))
-                    children.remove(STATE)
-                    break
-
-        if not in_list(children, STATE): continue
-
-        for i in range(1,4): #if an object travels it must be with the farmer
-            if state[i] != STATE[i] and STATE[0] == 'W':
-                children.remove(STATE)
-                break;
-
-    print("children: " +  str(children))
     return children
 
-def in_list(probe_list, STATE):
-    if STATE in probe_list:
-        return True
+def problem_constraints(child_states):
+    for STATE in child_states[:]:  
+        for i in range(1,4):
+            if STATE[1] == STATE[2] and STATE[2] != STATE[3] and STATE[0] != STATE[1]: # the wolf and the goat cannot be alone and goat and cabbage cannot be alone
+                child_states.remove(STATE)
+                break
+            if STATE[2] == STATE[3] and STATE[1] != STATE[2] and STATE[0] != STATE[2]:
+                child_states.remove(STATE)
+                break
 
+def check_amount_of_travelers(state, child_states):
+    for STATE in child_states[:]:  
+        travellers = 0
+        if STATE[0] == 'W' and state[0] == 'E':
+            for i in range(1,4):
+                if state[i] == 'W' and STATE[i] == 'E':
+                    print("removing oppsite " + str(state) + " " + str(STATE))
+                    child_states.remove(STATE)
+                    break
+                if state[i] != STATE[i]:
+                    travellers += 1 
+                    child_states.remove(STATE)
+                    break
+
+        if STATE[0] == 'E' and state[0] == 'W': 
+            for i in range(1,4):
+                if state[i] == 'E' and STATE[i] == 'W':
+                    travellers += 1 
+                    print("removing oppsite " + str(state) + " " + str(STATE))
+                    child_states.remove(STATE)
+                    break
+                if state[i] != STATE[i]:
+                    travellers += 1 
+                    child_states.remove(STATE)
+                    break
+        print("travellers: " + str(travellers))
+        if travellers > 1:
+            print("remove multiple travellers: " + str(STATE))
+            child_states.remove(STATE)
+
+        
+def check_farmer_place(state, child_states):
+    for STATE in child_states[:]:  
+        if state[0] == 'E': #if farmer is on east$ side, the subjects can't travel
+            for i in range(1,4):
+                if state[i] != STATE[i]:
+                    child_states.remove(STATE)
+                    break
+    for STATE in child_states[:]:  
+        if STATE[0] == 'W': #if farmer stays on west side, no subjects can travel
+            for i in range(1,4): 
+                if state[i] != STATE[i]:
+                    child_states.remove(STATE)
+                    break
 
 INITIAL_STATE = ('W', 'W', 'W', 'W') #We represent the a state with a tuple: (location, A status, B status)
 GOAL_STATE = ('E', 'E', 'E', 'E') 
